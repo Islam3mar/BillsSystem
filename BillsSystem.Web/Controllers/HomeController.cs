e4a8115e48f1defa6ee946db09400d30a4e91670
Softlaptop <Softlaptop@DESKTOP-65SBLOR>
2026-09-26 23:09:58 +0300

    Improve Design

--- BillsSystem.Web/Controllers/HomeController.cs
+++ BillsSystem.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using BillsSystem.Application.DTOs;
+using BillsSystem.Application.Interfaces;
 using BillsSystem.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,15 +10,66 @@ namespace BillsSystem.Web.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IBillService _billService;
+        private readonly IClientService _clientService;
+        private readonly IItemService _itemService;
+        private readonly ICompanyService _companyService;
+        private readonly ISalesReportService _salesReportService;
+
+        public HomeController(
+            IBillService billService,
+            IClientService clientService,
+            IItemService itemService,
+            ICompanyService companyService,
+            ISalesReportService salesReportService)
         {
-            return View();
+            _billService = billService;
+            _clientService = clientService;
+            _itemService = itemService;
+            _companyService = companyService;
+            _salesReportService = salesReportService;
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var bills = (await _billService.GetAllAsync()).ToList();
+            var clients = await _clientService.GetAllAsync();
+            var items = await _itemService.GetAllAsync();
+            var companies = await _companyService.GetAllCompaniesAsync();
+
+            var monthStart = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+            var monthResult = await _salesReportService.GenerateAsync(new SalesReportFilter
+            {
+                FromDate = monthStart,
+                ToDate = DateTime.Today
+            });
+
+            var vm = new HomeDashboardViewModel
+            {
+                TotalClients = clients.Count(),
+                TotalItems = items.Count(),
+                TotalCompanies = companies.Count(),
+                TotalBillsAllTime = bills.Count,
+                OutstandingAllTime = bills.Sum(b => b.TheRest),
+                MonthNetSales = monthResult.Success ? monthResult.Report!.TotalNetSales : 0,
+                MonthBillsCount = monthResult.Success ? monthResult.Report!.BillsCount : 0,
+                RecentBills = bills
+                    .OrderByDescending(b => b.BillDate).ThenByDescending(b => b.Id)
+                    .Take(5)
+                    .Select(b => new RecentBillItem
+                    {
+                        Id = b.Id,
+                        BillDate = b.BillDate,
+                        ClientName = b.Client.Name,
+                        TheNet = b.TheNet,
+                        TheRest = b.TheRest
+                    }).ToList()
+            };
+
+            return View(vm);
         }
+
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ BillsSystem.Web/ViewModels/HomeDashboardViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace BillsSystem.Web.ViewModels
+{
+    public class RecentBillItem
+    {
+        public int Id { get; set; }
+        public DateTime BillDate { get; set; }
+        public string ClientName { get; set; } = null!;
+        public decimal TheNet { get; set; }
+        public decimal TheRest { get; set; }
+    }
+
+    public class HomeDashboardViewModel
+    {
+        public int TotalClients { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalCompanies { get; set; }
+        public int TotalBillsAllTime { get; set; }
+        public decimal OutstandingAllTime { get; set; }
+
+        public decimal MonthNetSales { get; set; }
+        public int MonthBillsCount { get; set; }
+
+        public List<RecentBillItem> RecentBills { get; set; } = new();
+    }
+}
